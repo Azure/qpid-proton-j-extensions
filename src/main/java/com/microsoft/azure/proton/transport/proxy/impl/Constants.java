db79986cb0ff1516108682c457f9cff2a549acbd
Connie <conniey@microsoft.com>
2019-05-02 12:38:03 -0700

    Add additional constants.

--- src/main/java/com/microsoft/azure/proton/transport/proxy/impl/Constants.java
+++ src/main/java/com/microsoft/azure/proton/transport/proxy/impl/Constants.java
@@ -2,6 +2,8 @@ package com.microsoft.azure.proton.transport.proxy.impl;
 
 class Constants {
     static final String PROXY_AUTHENTICATE_HEADER = "Proxy-Authenticate:";
+    static final String PROXY_AUTHORIZATION = "Proxy-Authorization";
+
     static final String DIGEST = "Digest";
     static final String BASIC = "Basic";
 }
