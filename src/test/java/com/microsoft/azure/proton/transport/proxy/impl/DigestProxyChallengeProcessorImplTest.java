e51fb39e233c62096998b304f38ec390b804a45b
Connie <conniey@microsoft.com>
2019-05-04 21:53:05 -0700

    Adding validation class for digest validation.

--- src/test/java/com/microsoft/azure/proton/transport/proxy/impl/DigestProxyChallengeProcessorImplTest.java
+++ src/test/java/com/microsoft/azure/proton/transport/proxy/impl/DigestProxyChallengeProcessorImplTest.java
@@ -1,20 +1,37 @@
 package com.microsoft.azure.proton.transport.proxy.impl;
 
+import com.microsoft.azure.proton.transport.proxy.ProxyAuthenticationType;
+import com.microsoft.azure.proton.transport.proxy.ProxyConfiguration;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.net.*;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.*;
+import java.util.stream.Collectors;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static javax.xml.bind.DatatypeConverter.printHexBinary;
 
 public class DigestProxyChallengeProcessorImplTest {
-    private final String HOSTNAME = "127.0.0.1";
-    private final int PORT = 3128;
-    private final String USERNAME = "username";
-    private final String PASSWORD = "password";
+    private static final String NEW_LINE = "\r\n";
+    private static final String HOSTNAME = "127.0.0.1";
+    private static final int PORT = 3128;
+    private static final String USERNAME = "my-username";
+    private static final String PASSWORD = "my-password";
+    private static MessageDigest md5;
+
     private ProxySelector originalProxy;
+
+    @BeforeClass
+    public static void init() throws NoSuchAlgorithmException {
+        md5 = MessageDigest.getInstance(DigestProxyChallengeProcessorImpl.DEFAULT_ALGORITHM);
+    }
 
     @Before
     public void setup() {
@@ -51,13 +68,47 @@ public class DigestProxyChallengeProcessorImplTest {
 
     @Test
     public void testGetHeaderDigest() {
-        final String response = "HTTP/1.1 407 Proxy Authentication Required\r\n" +
-            "X-Squid-Error: ERR_CACHE_ACCESS_DENIED 0\r\n" +
-            "Proxy-Authenticate: Digest realm=\"Squid proxy-caching web server\", nonce=\"zWV5XAAAAAAgz1ACAAAAAE9hOwIAAAAA\", qop=\"auth\", stale=false\r\n" +
-            "Proxy-Authenticate: Basic realm=\"Squid proxy-caching web server\"\r\n" ;
+        // Arrange
+        final String realm = "Squid proxy-caching web server";
+        final String nonce = "zWV5XAAAAAAgz1ACAAAAAE9hOwIAAAAA";
+        final String qop = "auth";
 
-        final DigestProxyChallengeProcessorImpl proxyChallengeProcessor = new DigestProxyChallengeProcessorImpl("", response, new ProxyAuthenticator());
+        // a1 and a2 are hash values generated as an intermediate step in the Digest algorithm.
+        final String a1 = "6ea8f6c05777aae5987035155da478bc";
+        final String a2 = "9b1f7b831464108253ea5a0fd0ebb3d9";
+
+        final String response = generateProxyChallenge(realm, nonce, qop);
+        final DigestValidator validator = new DigestValidator(USERNAME, realm, nonce, "00000001", HOSTNAME, qop);
+
+        // Act
+        final DigestProxyChallengeProcessorImpl proxyChallengeProcessor =
+                new DigestProxyChallengeProcessorImpl(HOSTNAME, response, new ProxyAuthenticator());
         Map<String, String> headers = proxyChallengeProcessor.getHeader();
+
+        // Assert
+        String resp = headers.get(Constants.PROXY_AUTHORIZATION);
+        validator.assertEquals(resp, a1, a2);
+    }
+
+    @Test
+    public void testGetHeaderDigestWithProxy() {
+        // Arrange
+        final String realm = "My Test Realm";
+        final String nonce = "A randomly generated nonce";
+        final String qop = "auth";
+        final String response = generateProxyChallenge(realm, nonce, qop);
+
+        final String host = "foo.myhost.com";
+        final String username = "my-new-username";
+        final String password = "my-new-password";
+        final ProxyConfiguration configuration = new ProxyConfiguration(ProxyAuthenticationType.DIGEST, host, username, password);
+        final ProxyAuthenticator authenticator = new ProxyAuthenticator(configuration);
+
+        // Act
+        final DigestProxyChallengeProcessorImpl proxyChallengeProcessor =
+                new DigestProxyChallengeProcessorImpl("", response, authenticator);
+        Map<String, String> headers = proxyChallengeProcessor.getHeader();
+
         String resp = headers.get(Constants.PROXY_AUTHORIZATION);
         Assert.assertTrue(resp.contains("Digest "));
         Assert.assertTrue(resp.contains("username=\""));
@@ -68,5 +119,95 @@ public class DigestProxyChallengeProcessorImplTest {
         Assert.assertTrue(resp.contains("nc="));
         Assert.assertTrue(resp.contains("response=\""));
         Assert.assertTrue(resp.contains("qop=\""));
+    }
+
+    private static String generateProxyChallenge(String realm, String nonce, String qop) {
+        final String digest = String.format("%s %s realm=\"%s\", nonce=\"%s\", qop=\"%s\", stale=false",
+                Constants.PROXY_AUTHENTICATE_HEADER, Constants.DIGEST, realm, nonce, qop);
+        final String basic = String.format("%s %s realm=\"%s\"",
+                Constants.PROXY_AUTHENTICATE_HEADER, Constants.BASIC, realm);
+
+        return String.join(NEW_LINE,
+                "HTTP/1.1 407 Proxy Authentication Required",
+                "X-Squid-Error: ERR_CACHE_ACCESS_DENIED 0",
+                digest,
+                basic)
+                + NEW_LINE;
+    }
+
+    private class DigestValidator {
+        private static final String USERNAME_KEY = "username";
+        private static final String NONCE_KEY = "nonce";
+        private static final String URI_KEY = "uri";
+        private static final String CNONCE_KEY = "cnonce";
+        private static final String RESPONSE_KEY = "response";
+        private static final String NC_KEY = "nc";
+        private static final String QOP_KEY = "qop";
+
+        private final Map<String, String> expected = new HashMap<>();
+
+        DigestValidator(String username, String realm, String nonce, String nc, String uri, String qop) {
+            expected.put(NC_KEY, nc);
+            expected.put(USERNAME_KEY, username);
+            expected.put("realm", realm);
+            expected.put(NONCE_KEY, nonce);
+            expected.put(URI_KEY, uri);
+            expected.put(QOP_KEY, qop);
+            expected.put(CNONCE_KEY, null);
+            expected.put(RESPONSE_KEY, null);
+        }
+
+        void assertEquals(String response, String a1, String a2) {
+            Assert.assertNotNull(response);
+            Assert.assertTrue(response.startsWith(Constants.DIGEST));
+
+            String[] split = response.substring(Constants.DIGEST.length()).split(",");
+
+            Map<String, String> actual = Arrays.stream(split).map(p -> {
+                String[] part = p.split("=");
+                Assert.assertEquals(2, part.length);
+
+                String key = part[0].trim();
+                String value = part[1].replace("\"", "").trim();
+                return new Pair(key, value);
+            }).collect(Collectors.toMap(Pair::key, Pair::value));
+
+            Assert.assertEquals(expected.size(), actual.size());
+
+            expected.forEach((key, value) -> {
+                // Skipping "cnonce" and "response" because they are randomly generated each time. Later on, we'll
+                // check for the response's validity.
+                if (key.equals(CNONCE_KEY) || key.equals(RESPONSE_KEY)) {
+                    return;
+                }
+
+                Assert.assertTrue(actual.containsKey(key));
+                Assert.assertEquals(value, actual.get(key));
+            });
+
+            String expectedRawResponse = String.join(":",
+                    a1, expected.get(NONCE_KEY), expected.get(NC_KEY), actual.get(CNONCE_KEY), expected.get(QOP_KEY), a2);
+            String expectedResponse = printHexBinary(md5.digest(expectedRawResponse.getBytes(UTF_8))).toLowerCase(Locale.ROOT);
+
+            Assert.assertEquals(expectedResponse, actual.get(RESPONSE_KEY));
+        }
+    }
+
+    private static class Pair {
+        private final String key;
+        private final String value;
+
+        Pair(String key, String value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        String key() {
+            return this.key;
+        }
+
+        String value() {
+            return this.value;
+        }
     }
 }
