b1cc89944a8058547d6a8f7b6411dc851ccd3bfa
Connie <conniey@microsoft.com>
2019-05-09 23:40:55 -0700

    Adding DigestUtils.convertToHexString

---
+++ src/main/java/com/microsoft/azure/proton/transport/proxy/impl/DigestUtils.java
@@ -0,0 +1,21 @@
+package com.microsoft.azure.proton.transport.proxy.impl;
+
+class DigestUtils {
+    private final static char[] HEX_CHARACTERS = "0123456789abcdef".toCharArray();
+
+    /**
+     * Converts a byte array to its hex string representation.
+     * Taken from: https://stackoverflow.com/a/9855338
+     */
+    static String convertToHexString(byte[] bytes) {
+        final char[] hexChars = new char[bytes.length * 2];
+
+        for (int j = 0; j < bytes.length; j++) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = HEX_CHARACTERS[v >>> 4];
+            hexChars[j * 2 + 1] = HEX_CHARACTERS[v & 0x0F];
+        }
+
+        return new String(hexChars);
+    }
+}
