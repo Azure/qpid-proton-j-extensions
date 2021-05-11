1deac82dbded58a5618416c5bf5564f94bd4bb30
Connie <conniey@microsoft.com>
2021-05-10 17:14:20 -0700

    Adding package-info to all classes.

---
+++ src/main/java/com/microsoft/azure/proton/transport/proxy/impl/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Package containg implementation for {@link com.microsoft.azure.proton.transport.proxy.ProxyHandler} and {@link
+ * com.microsoft.azure.proton.transport.proxy.Proxy}.
+ */
+package com.microsoft.azure.proton.transport.proxy.impl;
