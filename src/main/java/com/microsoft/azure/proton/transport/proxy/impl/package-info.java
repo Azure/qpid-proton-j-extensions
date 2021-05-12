27994bfe683bcfc3831aacc748ede32c37618d1d
Connie <conniey@microsoft.com>
2021-05-12 16:47:42 -0700

    Add headers to all src files.

--- src/main/java/com/microsoft/azure/proton/transport/proxy/impl/package-info.java
+++ src/main/java/com/microsoft/azure/proton/transport/proxy/impl/package-info.java
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 /**
  * Package containg implementation for {@link com.microsoft.azure.proton.transport.proxy.ProxyHandler} and {@link
  * com.microsoft.azure.proton.transport.proxy.Proxy}.
--- src/main/java/com/microsoft/azure/proton/transport/ws/impl/package-info.java
+++ src/main/java/com/microsoft/azure/proton/transport/ws/impl/package-info.java
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 /**
  * Package containg implementation for {@link com.microsoft.azure.proton.transport.ws.WebSocketHandler} and {@link
  * com.microsoft.azure.proton.transport.ws.WebSocket}.
