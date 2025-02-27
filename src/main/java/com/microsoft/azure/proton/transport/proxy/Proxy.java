c2706fbb96847a4efa4224c4345be3e034586271
Connie Yau <conniey@microsoft.com>
2025-02-27 12:33:44 -0800

    Add documentation for Proxy.

--- src/main/java/com/microsoft/azure/proton/transport/proxy/Proxy.java
+++ src/main/java/com/microsoft/azure/proton/transport/proxy/Proxy.java
@@ -15,11 +15,29 @@ public interface Proxy {
      * States that the proxy can be in.
      */
     enum ProxyState {
+        /**
+         * 1. No connection to proxy has been made.
+         */
         PN_PROXY_NOT_STARTED,
+        /**
+         * 2. Sent initial CONNECT request to proxy.
+         */
         PN_PROXY_CONNECTING,
+        /**
+         * 3. Proxy has responded with a Proxy-Authorization challenge.
+         */
         PN_PROXY_CHALLENGE,
+        /**
+         * 4. Proxy has responded with a Proxy-Authorization challenge.
+         */
         PN_PROXY_CHALLENGE_RESPONDED,
+        /**
+         * 5. Proxy has responded with a Proxy-Authorization challenge.
+         */
         PN_PROXY_CONNECTED,
+        /**
+         * There has been a failure connecting to proxy.
+         */
         PN_PROXY_FAILED
     }
 
